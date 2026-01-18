3bd8da356c9255b93c5565e28b7ecdfddeaeedf5
Yang <1312835064@qq.com>
2026-01-18 15:46:12 +0800

    Initial Commit

---
+++ PortfolioPilot/PortfolioPilotApp.swift
@@ -0,0 +1,32 @@
+//
+//  PortfolioPilotApp.swift
+//  PortfolioPilot
+//
+//  Created by Liuyang on 2026/1/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PortfolioPilotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
